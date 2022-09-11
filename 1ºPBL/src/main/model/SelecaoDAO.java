8af2037bbd16fb2294f9ecefcd000c82fd8baed6
TheShadowAge <oliveiraiangamade@gmail.com>
2022-09-10 23:48:37 -0300

    v20220910.2
    Criado a interface do DAO da Seleção

---
+++ 1ºPBL/src/main/model/SelecaoDAO.java
@@ -0,0 +1,10 @@
+package main.model;
+import java.util.List;
+
+public interface SelecaoDAO {
+	public void create(Selecao selecao);
+	public Selecao read(int id);
+	public List<Selecao> readAll();
+	public void update(int id, int opcao, String atributo);
+	public void delete(int id);
+}
