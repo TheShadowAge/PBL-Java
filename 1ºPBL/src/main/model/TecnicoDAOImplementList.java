31a19e19a610bbd32b4d386aceb009d5973740c4
Gabriel Sena <113054109+Gabriel-SBS@users.noreply.github.com>
2022-09-11 23:13:53 -0300

    v20220911.9
    - Criação do DAO de Técnico com create, read, readAll, update, delete e getContagem com funcionamento idêntico ao do DAO de seleção
    
    - Atualização do main, acrescentando alguns textos para os submenus

---
+++ 1ºPBL/src/main/model/TecnicoDAOImplementList.java
@@ -0,0 +1,58 @@
+package main.model;
+
+import java.util.*;
+
+public class TecnicoDAOImplementList implements TecnicoDAO {
+
+	int contagemIds = 0;
+	private Map<Integer, Tecnico> tecnicos = new HashMap<Integer, Tecnico>();
+	
+	@Override
+	public void create(Tecnico tecnico) {
+		tecnicos.put(tecnico.getId(), tecnico);
+		contagemIds++;
+	}
+
+	@Override
+	public Tecnico read(int id) {
+		return tecnicos.get(id);
+	}
+
+	@Override
+	public List<Tecnico> readAll() {
+		List<Tecnico> listTecnico = new LinkedList<Tecnico>();
+		for (Tecnico tecnicosIterator: tecnicos.values()) {
+			listTecnico.add(tecnicosIterator);
+		}
+		return listTecnico;
+	}
+
+	@Override
+	public void update(int id, int opcao, String atributo) {
+		switch(id) {
+		case 1:
+			tecnicos.get(id).setNome(atributo);
+			break;
+		case 2:
+			tecnicos.get(id).setIdade(Integer.parseInt(atributo));
+			break;
+		case 3:
+			tecnicos.get(id).setNacionalidade(atributo);
+			break;
+		case 4:
+			tecnicos.get(id).setTimeAnterior(atributo);
+		}
+
+	}
+
+	@Override
+	public void delete(int id) {
+		tecnicos.remove(id);
+	}
+	
+	@Override
+	public int getContagem() {
+		return contagemIds;
+	}
+
+}
