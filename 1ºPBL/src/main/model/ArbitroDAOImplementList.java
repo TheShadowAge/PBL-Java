10067e536292dbdcec766066dbb384fd7099c000
Gabriel Sena <113054109+Gabriel-SBS@users.noreply.github.com>
2022-09-26 18:28:55 -0300

    GS20220926.4
    Conclusão do Javadoc da implementação da interface ArbitroDAOImplementList

--- 1ºPBL/src/main/model/ArbitroDAOImplementList.java
+++ 1ºPBL/src/main/model/ArbitroDAOImplementList.java
@@ -5,20 +5,45 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+
+/**
+ * Classe que implementa a interface ArbitroDAO.
+ * @see main.model.ArbitroDAO
+ * @author Gabriel Sena
+ * @author Ian Gabriel
+ *
+ */
+
 public class ArbitroDAOImplementList implements ArbitroDAO {
 
 	private Map<Integer, Arbitro> arbitros = new HashMap<Integer, Arbitro>();
+	
+	/**
+	 * Método que cria um árbitro no sistema.
+	 * @param arbitro O objeto do tipo Arbitro que será criado.
+	 */
 	
 	@Override
 	public void create(Arbitro arbitro) {
 		arbitros.put(arbitro.getId(), arbitro);
 	}
 
+	/**
+	 * Método que retorna um objeto do tipo Arbitro pelo id do Arbitro.
+	 * @param id Inteiro que representa o ID que será usado para acessar o Arbitro.
+	 * @return Retorna o árbitro do ID especificado.
+	 */
+	
 	@Override
 	public Arbitro read(int id) {
 		return arbitros.get(id);
 	}
 
+	/**
+	 * Metódo que retorna uma lista com todos os árbitros do sistema.
+	 * @return Retorna uma lista com todos os árbitros do sistema.
+	 */
+	
 	@Override
 	public List<Arbitro> readAll() {
 		List<Arbitro> listArbitro = new LinkedList<Arbitro>();
@@ -28,6 +53,13 @@ public class ArbitroDAOImplementList implements ArbitroDAO {
 		return listArbitro;
 	}
 
+	/**
+	 * Método que atualiza as informações dos árbitros do sistema. 
+	 * @param id Inteiro que representa o ID do árbitro a ser atualizado. 
+	 * @param opcao Inteiro que define a informação que será atualizada. 1 para nome, 2 para idade, 3 para nacionalidade e 4 para o tipo.
+	 * @param atributo Uma String com o atributo que será atualizado.
+	 */
+	
 	@Override
 	public void update(int id, int opcao, String atributo) {
 		switch(opcao) {
@@ -46,6 +78,11 @@ public class ArbitroDAOImplementList implements ArbitroDAO {
 
 	}
 
+	/**
+	 * Método que deleta um árbitro do Sistema pelo ID.
+	 * @param id Inteiro que representa o ID do árbitro que será removido.
+	 */
+	
 	@Override
 	public void delete(int id) {
 		arbitros.put(id, null);
