7f8581b4b1c47043518e782c898e1e6d106a8ddc
Gabriel Sena <113054109+Gabriel-SBS@users.noreply.github.com>
2022-09-26 19:44:57 -0300

    GS20220926.6
    Terminados os Javadoc de Arbitro, Tecnico, StartingEntitiesCreator, Jogador

--- 1ºPBL/src/main/model/Jogador.java
+++ 1ºPBL/src/main/model/Jogador.java
@@ -1,5 +1,11 @@
 package main.model;
 
+/**
+ * Classe que representa os jogadores do sistema.
+ * @author Gabriel Sena
+ * @author Ian Gabriel
+ */
+
 public class Jogador extends PessoaSelecao{
 	private String posicao;
 	private String codJog;
@@ -7,6 +13,19 @@ public class Jogador extends PessoaSelecao{
 	private int cartaoVermelho;
 	private int golsQuantidade;
 	private boolean titular;
+	
+	/**
+	 * Construtor da classe jogador.
+	 * @param nome Nome do jogador.
+	 * @param selecao Seleção do jogador.
+	 * @param nacionalidade Nacionalidade do jogador.
+	 * @param idade Idade do jogador.
+	 * @param posicao Posição do jogador.
+	 * @param cartaoAmarelo Quantidade de cartões amarelos do jogador.
+	 * @param cartaoVermelho Quantidade de cartões vermelhos do jogador.
+	 * @param titular Mostrar se o jogador é titular ou não
+	 * @param golsQuantidade Quantidade de gols do jogador.
+	 */
 	
 	public Jogador(String nome, int selecao, String nacionalidade, int idade, String posicao, int cartaoAmarelo, int cartaoVermelho, int golsQuantidade, boolean titular){
 		this.nome = nome;
@@ -20,6 +39,11 @@ public class Jogador extends PessoaSelecao{
 		this.golsQuantidade = golsQuantidade;
 	}
 	
+	/**
+	 * Método que retorna as informações do jogador quando printar o objeto.
+	 * @return Retorna uma String com as informações do jogador.
+	 */
+	
 	public String toString() {
 		String titularSimNao = "Nao";
 		if (titular) {
@@ -28,50 +52,110 @@ public class Jogador extends PessoaSelecao{
 		return "ID do jogador: "+ id + "\nCodigo de jogador: " + codJog + "\nNome do jogador: " + nome + "\nSelecao do jogador: " + selecao + "\nNacionalidade do jogador: " + nacionalidade + "\nIdade do jogador: " + idade + "\nPosicao do jogador: " + posicao + "\nCartoes Amarelos do jogador: " + cartaoAmarelo + "\nCartoes Vermelhos do jogador: " + cartaoVermelho + "\nO jogador e titular? " + titularSimNao + "\nQuantidade de Gols do jogador: " + golsQuantidade;
 	}
 	
+	/**
+	 * Retorna a posição do jogador.
+	 * @return Retorna o Inteiro com a posição do jogador.
+	 */
+	
 	public String getPosicao() {
 		return posicao;
 	}
+	
+	/**
+	 * Atualiza a posição do jogador.
+	 * @param posicao Inteiro com a posição nova do jogador.
+	 */
 	
 	public void setPosicao(String posicao) {
 		this.posicao = posicao;
 	}
 	
+	/**
+	 * Retorna a quantidade de cartões amarelos do jogador.
+	 * @return Retorna o Inteiro com a quantidade de cartões amarelos do jogador.
+	 */
+	
 	public int getCartaoAmarelo() {
 		return cartaoAmarelo;
 	}
+	
+	/**
+	 * Atualiza a quantidade de cartões amarelos do jogador.
+	 * @param cartaoAmarelo Inteiro com a quantidade de cartões amarelos do jogador.
+	 */
 	
 	public void setCartaoAmarelo(int cartaoAmarelo) {
 		this.cartaoAmarelo = cartaoAmarelo;
 	}
 	
+	/**
+	 * Retorna a quantidade de cartões vermelhos do jogador.
+	 * @return Retorna o Inteiro com a quantidade de cartões vermelhos do jogador.
+	 */
+	
 	public int getCartaoVermelho() {
 		return cartaoVermelho;
 	}
+	
+	/**
+	 * Atualiza a quantidade de cartões vermelhos do jogador.
+	 * @param cartaoVermelho Inteiro com a quantidade de cartões vermelhos do jogador.
+	 */
 	
 	public void setCartaoVermelho(int cartaoVermelho) {
 		this.cartaoVermelho = cartaoVermelho;
 	}
 	
+	/**
+	 * Retorna a quantidade de gols do jogador.
+	 * @return Retorna o Inteiro com a quantidade de gols do jogador.
+	 */
+	
 	public int getGolsQuantidade() {
 		return golsQuantidade;
 	}
+	
+	/**
+	 * Atualiza a quantidade de gols do jogador.
+	 * @param golsQuantidade Inteiro com a quantidade de gols do jogador.
+	 */
 	
 	public void setGolsQuantidade(int golsQuantidade) {
 		this.golsQuantidade = golsQuantidade;
 	}
 	
+	/**
+	 * Retorna se o jogador é titular ou não.
+	 * @return Retorna um boleano se o jogador é titular (true) ou não (false).
+	 */
+	
 	public boolean isTitular() {
 		return titular;
 	}
+	
+	/**
+	 * Atualiza se o jogador é titular ou não.
+	 * @param titular boleano se o jogador é titular (true) ou não (false).
+	 */
 	
 	public void setTitular(boolean titular) {
 		this.titular = titular;
 	}
 
+	/**
+	 * Retorna o codigo de jogador do jogador.
+	 * @return Retorna uma String com o codigo do jogador.
+	 */
+	
 	public String getCodJog() {
 		return codJog;
 	}
 
+	/**
+	 * Atualiza o codigo de jogador do jogador.
+	 * @param codJog uma String com o codigo do jogador.
+	 */
+	
 	public void setCodJog(String codJog) {
 		this.codJog = codJog;
 	}
--- 1ºPBL/src/main/model/Tecnico.java
+++ 1ºPBL/src/main/model/Tecnico.java
@@ -1,7 +1,23 @@
 package main.model;
 
+
+/**
+ * Classe que representa os Tecnicos do sistema.
+ * @author Gabriel Sena
+ * @author Ian Gabriel
+ */
+
 public class Tecnico extends PessoaSelecao{
 	private String timeAnterior;
+	
+	/**
+	 * Construtor da classe Tecnico.
+	 * @param nacionalidade Nacionalidade do tecnico.
+	 * @param timeAnterior Time anterior do Tecnico.
+	 * @param nome Nome do tecnico.
+	 * @param idade Idade do tecnico.
+	 * @param selecao Selecao do tecnico.
+	 */
 	
 	public Tecnico(String nacionalidade, int selecao, String timeAnterior, String nome, int idade){
 		this.nacionalidade = nacionalidade;
@@ -11,12 +27,29 @@ public class Tecnico extends PessoaSelecao{
 		this.selecao = selecao;
 	}
 	
+	/**
+	 * Retorna o time anterior do Tecnico.
+	 * @return Retorna um String com o time anterior do tecnico.
+	 */
+	
 	public String getTimeAnterior() {
 		return timeAnterior;
 	}
+	
+	/**
+	 * Atualiza o time anterior do tecnico.
+	 * @param timeAnterior String com o time anterior do tecnico.
+	 */
+	
 	public void setTimeAnterior(String timeAnterior) {
 		this.timeAnterior = timeAnterior;
 	}
+	
+	
+	/**
+	 * Método que retorna as informações do tecnico quando printar o objeto.
+	 * @return Retorna uma String com as informações do tecnico.
+	 */
 	
 	public String toString() {
 		return "ID do tecnico: "+ id + "\nNome do tecnico: " + nome + "\nSelecao do tecnico: " + selecao + "\nNacionalidade do tecnico: " + nacionalidade + "\nIdade do tecnico: " + idade + "\nTime Anterior do tecnico: " + timeAnterior;
