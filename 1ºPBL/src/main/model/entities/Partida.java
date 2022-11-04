b5b894c8170a0da06a6fa71e2ff85d84fc73bf33
TheShadowAge <oliveiraiangamade@gmail.com>
2022-11-04 17:46:35 -0300

    ???????????????

--- 1ºPBL/src/main/model/entities/Partida.java
+++ 1ºPBL/src/main/model/entities/Partida.java
@@ -164,4 +164,22 @@ public class Partida extends Entidade {
 			this.jogsCarts1 = jogsCarts1;
 		}
 
+		public int getGolsTime1() {
+			return golsTime1;
+		}
+
+		public void setGolsTime1(int golsTime1) {
+			this.golsTime1 = golsTime1;
+		}
+
+		public int getGolsTime2() {
+			return golsTime2;
+		}
+
+		public void setGolsTime2(int golsTime2) {
+			this.golsTime2 = golsTime2;
+		}
+
+		
+
 }
