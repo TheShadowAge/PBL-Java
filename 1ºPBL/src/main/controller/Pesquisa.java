1ae6b5bda6fefc2e9b3a61c7fad7837bf6702950
TheShadowAge <oliveiraiangamade@gmail.com>
2022-11-07 15:35:43 -0300

    Vírgula aleatória

--- 1ºPBL/src/main/controller/Pesquisa.java
+++ 1ºPBL/src/main/controller/Pesquisa.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import main.model.DAO.*;
-import main.model.entities.*;,
+import main.model.entities.*;
 
 /**
  * classe de metodos de pesquisa.
