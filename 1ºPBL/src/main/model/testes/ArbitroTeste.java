1ef732b97b1299e74c527765802c64a24e398f1f
TheShadowAge <oliveiraiangamade@gmail.com>
2022-11-07 17:11:47 -0300

    Exemplo completo de teste de classe

--- 1ºPBL/src/main/model/testes/ArbitroTeste.java
+++ 1ºPBL/src/main/model/testes/ArbitroTeste.java
@@ -20,5 +20,33 @@ class ArbitroTeste {
 		Entidade.resetContagem();
 		Assert.assertEquals(arbitro, arbitroDAO.read(1));
 	}
+	
+	@Test
+	void testAcessarErrado() {
+		ArbitroDAO arbitroDAO = new ArbitroDAOImplementList();
+		Assert.assertNull(arbitroDAO.read(0));
+	}
+	
+	@Test
+	void testUpdate() {
+		ArbitroDAO arbitroDAO = new ArbitroDAOImplementList();
+		Arbitro arbitro = new Arbitro("Jeferson", "Brasileiro", "Juiz", 52);
+		arbitroDAO.create(arbitro);
+		Entidade.resetContagem();
+		arbitroDAO.update(1, 2, "23");
+		Assert.assertEquals(23, arbitroDAO.read(1).getIdade());
+	}
+	
+	@Test
+	void testDelete() {
+		ArbitroDAO arbitroDAO = new ArbitroDAOImplementList();
+		Arbitro arbitro = new Arbitro("Jeferson", "Brasileiro", "Juiz", 52);
+		arbitroDAO.create(arbitro);
+		Entidade.resetContagem();
+		int id = arbitroDAO.read(1).getId();
+		arbitroDAO.delete(id);
+		Assert.assertNull(arbitroDAO.read(id));
+		
+	}
 
 }
