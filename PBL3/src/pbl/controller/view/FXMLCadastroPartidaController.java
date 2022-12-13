7563c49dc1b4130aa0703438cd9f4c2a8b32cb6c
TheShadowAge <oliveiraiangamade@gmail.com>
2022-12-12 21:56:01 -0300

    AAAAAAAAAAAAAAH

---
+++ PBL3/src/pbl/controller/view/FXMLCadastroPartidaController.java
@@ -0,0 +1,116 @@
+package pbl.controller.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class FXMLCadastroPartidaController {
+
+    @FXML
+    private Button ButtonAlterarSelecao;
+
+    @FXML
+    private Button ButtonInserirSelecao;
+
+    @FXML
+    private Button ButtonLimparPesquisa;
+
+    @FXML
+    private Button ButtonPesquisar;
+
+    @FXML
+    private Button ButtonRemoverSelecao;
+
+    @FXML
+    private TableView<?> TableViewPartida;
+
+    @FXML
+    private TableView<?> TableViewSelecaoJogador;
+
+    @FXML
+    private TableView<?> TableViewSelecaoPartidas;
+
+    @FXML
+    private TextField TextFieldPesquisa;
+
+    @FXML
+    private Label labelLocalPartida;
+
+    @FXML
+    private Label labelPartidaData;
+
+    @FXML
+    private Label labelPartidaHorário;
+
+    @FXML
+    private Label labelPartidaID;
+
+    @FXML
+    private Label labelPartidaNome;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnPartidaJogadorCAmar;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnPartidaJogadorCVerm;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnPartidaJogadorGol;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnPartidaJogadorNome;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnPartidaJogadores;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnSelecaoID;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnSelecaoNome;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnSelecaoPartida;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnSelecaoPartidaGol;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnSelecaoPartidaNome;
+
+    @FXML
+    private TableColumn<?, ?> tablePartidaSelecaoCAmar;
+
+    @FXML
+    private TableColumn<?, ?> tablePartidaSelecaoCVerm;
+
+    @FXML
+    void handleButtonAlterarSelecao(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButtonInserirSelecao(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButtonLimpar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButtonPesquisar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButtonRemoverSelecao(ActionEvent event) {
+
+    }
+
+}
